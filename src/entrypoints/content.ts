07f9800383dec762157c03b685087db6667182db
Ege Feyzioglu <egefeyzioglu@yahoo.com>
2026-04-04 20:05:34 -0400

    Formatting

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,21 +1,26 @@
 export default defineContentScript({
-  matches: ['https://efeyzee.dev/*'],
+  matches: ["https://efeyzee.dev/*"],
   main() {
     const abortController = new AbortController();
 
     // Event listener to trigger the command dropdown when the user types `/`
     // into an <input> or <textarea>
     // TODO: Support shadow DOMs, probably with a MutationObserver
-    document.addEventListener('keydown', slashCommandKeydownHandler, {
-      signal: abortController.signal
-    })
+    document.addEventListener("keydown", slashCommandKeydownHandler, {
+      signal: abortController.signal,
+    });
     return () => abortController.abort();
   },
 });
 
 function slashCommandKeydownHandler(e: KeyboardEvent) {
-  if (e.key === '/') {
-    if (!(e.target instanceof HTMLInputElement || e.target instanceof HTMLTextAreaElement)) {
+  if (e.key === "/") {
+    if (
+      !(
+        e.target instanceof HTMLInputElement ||
+        e.target instanceof HTMLTextAreaElement
+      )
+    ) {
       // TODO: Support editable div's in the future
       return;
     }
@@ -27,10 +32,10 @@ function slashCommandKeydownHandler(e: KeyboardEvent) {
       return;
     }
 
-    const lineStart = value.lastIndexOf('\n', cursor - 1) + 1;
+    const lineStart = value.lastIndexOf("\n", cursor - 1) + 1;
     const lineText = value.slice(lineStart, cursor);
 
-    const isEmptyLine = lineText.trim() === '';
+    const isEmptyLine = lineText.trim() === "";
     const hasSpaceBefore = cursor > 0 && /\s/.test(value[cursor - 1]);
 
     // Only show the dropdown if the `/` is its own word
